3208b21379e65da338b8a026a2cc152fcdd6db84
zioscuro <lore.rang@gmail.com>
2022-11-01 15:36:56 +0100

    Add hero section animated background

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello');
+VANTA.WAVES({
+  el: '#personal-info', 
+  color: 0x193d47,
+  waveHeight: 10,
+  shininess: 20,
+  waveSpeed: 0.3,
+  zoom: 0.75
+})
+
+window.dispatchEvent(new Event('resize'));
+effect.resize()
