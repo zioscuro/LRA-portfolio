7b40ea5907c640088a66a8650d30888294a151b6
zioscuro <lore.rang@gmail.com>
2022-11-09 22:39:58 +0100

    Update hamburger menu to generate child nodes dynamically based on nav links

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // Hero section background effect
 const effect = VANTA.WAVES({
-  el: '#personal-info', 
+  el: '#personal-info',
   color: 0x193d47,
   waveHeight: 10,
   shininess: 20,
   waveSpeed: 0.3,
-  zoom: 0.75
-})
+  zoom: 0.75,
+});
 
 setTimeout(() => {
-  effect.resize()
-}, 100)
+  effect.resize();
+}, 100);
 
 // hamburger menu
-const menuBtn = document.querySelector('#hamburger-button')
-const hamburgerMenu = document.querySelector('#hamburger-menu')
+const menuBtn = document.querySelector('#hamburger-button');
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const navLinks = document.querySelectorAll('.nav-right > .menu-link');
+
+navLinks.forEach((link) => {
+  const navLink = document.createElement('a');
+  navLink.setAttribute('href', link.href);
+  navLink.textContent = link.textContent;
+
+  hamburgerMenu.appendChild(navLink);
+});
 
 menuBtn.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('hidden')
-})
+  hamburgerMenu.classList.toggle('hidden');
+});
